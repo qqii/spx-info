b8cbccf9c5f3b5bd053682cd5aa7e2628ffc6cff
qqii <QiruiChen97@gmail.com>
2025-05-04 17:38:00 +0100

    feat: load s&p 500 data from yahoo finance

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from spx-info!")
+from pathlib import Path
 
+import yfinance as yf
+import pandas as pd
 
-if __name__ == "__main__":
-    main()
+csv_file = Path("sp500.csv")
+if not csv_file.exists():
+    df = yf.download("^GSPC")
+
+    df = df.reset_index()  # Use an integer index
+    df.columns = df.columns.droplevel(1)  # Remove the second level (ticker)
+    df.to_csv(csv_file)
+else:
+    df = pd.read_csv(csv_file, parse_dates=True)
+
+# Calculate the running maximum price
+df["Running Max"] = df["Close"].cummax()
+df["Drawdown"] = (df["Close"] - df["Running Max"]) / df["Running Max"]
+
+downturn_days = df[df["Drawdown"] <= -0.10]
+downturn_days = downturn_days[["Date", "Close", "Running Max", "Drawdown"]]
+
+print("Days with more than 10% downturn from peak:")
+print(downturn_days)
